aec200b43211f4e2c1df8c3bb85360a280baf72e
Humoyunmirzo <humoyinmirzogafforov@gmail.com>
2025-02-03 17:22:06 +0500

    change code property nullbal

--- Domain/EntityDto/CreateUserDto.cs
+++ Domain/EntityDto/CreateUserDto.cs
@@ -9,8 +9,8 @@ namespace Domain.EntityDto
     public  class CreateUserDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; }  = null!;
+        public string Password { get; set; } = null!;
     }
 }
